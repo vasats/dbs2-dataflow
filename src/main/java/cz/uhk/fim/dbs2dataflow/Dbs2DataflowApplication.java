41c76016ead9450ace53244a25f7186b512b0bf9
vasats <mirek.vasata@seznam.cz>
2024-03-07 15:47:40 +0100

    Initial commit

---
+++ src/main/java/cz/uhk/fim/dbs2dataflow/Dbs2DataflowApplication.java
@@ -0,0 +1,13 @@
+package cz.uhk.fim.dbs2dataflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dbs2DataflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dbs2DataflowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/uhk/fim/dbs2dataflow/Dbs2DataflowApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.uhk.fim.dbs2dataflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dbs2DataflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
