018c64d87d82a01fa4617acda8514496ce7d62fa
vasats <vasatmi1@uhk.cz>
2024-03-19 19:25:27 +0100

    added repositories

---
+++ src/main/java/cz/uhk/fim/dbs2dataflow/repository/WebovyUcetRepository.java
@@ -0,0 +1,12 @@
+package cz.uhk.fim.dbs2dataflow.repository;
+
+import cz.uhk.fim.dbs2dataflow.model.WebovyUcet;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Repository
+public interface WebovyUcetRepository extends CrudRepository<WebovyUcet,Integer> {
+
+    public WebovyUcet findWebovyUcetByJmeno(String jmeno);
+}
---
+++ src/main/java/cz/uhk/fim/dbs2dataflow/repository/ZaznamOSpotrebeRepository.java
@@ -0,0 +1,8 @@
+package cz.uhk.fim.dbs2dataflow.repository;
+
+import cz.uhk.fim.dbs2dataflow.model.ZaznamOSpotrebe;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ZaznamOSpotrebeRepository extends CrudRepository<ZaznamOSpotrebe,Integer> {
+
+}
