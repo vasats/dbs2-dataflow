24a66312c4d1378be39b40ddc0df2bb6531ff47a
vasats <mirek.vasata@seznam.cz>
2024-03-21 17:35:24 +0100

    added repositories for SpotrebaInfo and Zamestnanec

---
+++ src/main/java/cz/uhk/fim/dbs2dataflow/repository/SpotrebaInfoViewRepository.java
@@ -0,0 +1,6 @@
+package cz.uhk.fim.dbs2dataflow.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface SpotrebaInfoViewRepository extends CrudRepository<> {
+}
