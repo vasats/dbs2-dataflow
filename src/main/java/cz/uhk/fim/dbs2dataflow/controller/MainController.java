f969718f562e9f66cbac1f144d5c99817e3c9e15
vasats <vasatmi1@uhk.cz>
2024-03-27 21:59:28 +0100

    added names to model attributes

--- src/main/java/cz/uhk/fim/dbs2dataflow/controller/MainController.java
+++ src/main/java/cz/uhk/fim/dbs2dataflow/controller/MainController.java
@@ -2,6 +2,7 @@ package cz.uhk.fim.dbs2dataflow.controller;
 
 
 import cz.uhk.fim.dbs2dataflow.exception.DataNotFoundException;
+import cz.uhk.fim.dbs2dataflow.model.Tovarna;
 import cz.uhk.fim.dbs2dataflow.model.WebovyUcet;
 import cz.uhk.fim.dbs2dataflow.service.SpotrebaService;
 import cz.uhk.fim.dbs2dataflow.service.TovarnaService;
@@ -10,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -25,20 +25,19 @@ public class MainController {
     @Autowired
     TovarnaService tovarnaService;
     @GetMapping(value = "/main")
-    public String showMainPage(){
-
-
+    public String showMainPage(Model model){
+        List<Tovarna> tovarnaList = tovarnaService.getAll();
+        model.addAttribute("tovarnaList", tovarnaList);
         return "index";
     }
 
     @GetMapping(value = "/tovarna/{id}")
     private String showFactoryInfo(@PathVariable("id") Integer id, Model model){
         try {
-            model.addAttribute(tovarnaService.getTovarnaById(id));
+            model.addAttribute("tovarna",tovarnaService.getTovarnaById(id));
         } catch (DataNotFoundException e){
 
         }
-
         return "tovarna";
     }
 
